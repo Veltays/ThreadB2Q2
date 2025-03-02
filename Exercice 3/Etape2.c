1503861a23169fdf26e7c178fd3632fbdf5e2e8b
Veltays <yassine.gran@student.hepl.be>
2025-03-02 22:00:52 +0100

    Exo3 fini bonus en cours

--- Exercice 3/Etape2.c
+++ Exercice 3/Etape2.c
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <string.h>
 
+
 typedef struct // création d'une structure donnée, qui aura un nom et un nombre de seconde
 {
     char nom[20];
@@ -23,29 +24,38 @@ DONNEE Param; // Lui qui va être utiliser pour des création
 
 
 pthread_t ThreadSecondaire[4];          //Handler Des thread Secondaire
+
+pthread_mutex_t MutexParam;
+
+
+
 void *fonctionThread(DONNEE *param);    //Fonction qui va être éxécuté par les différents thread
 
 int main()
 {
-    /*
+    /* 
      * On copie la structure de DONNEE[i] dans une variable nommer param,
      * Création des 4 threads secondaire en envoyant cette structure param
+     * On lock les thread le temp d'afficher le thread qui vient de rentrer
      * on attend la mort des 4 thread lancé en ne récupérant aucun paramètre
      * on quitte le programme avec return 0
     */
 
+    pthread_mutex_init(&MutexParam,NULL);
     fprintf(stderr,"Création des Threads secondaire \n\n");
     for (int i = 0; i < 4; i++)
     {
+        pthread_mutex_lock(&MutexParam);
         memcpy(&Param, &data[i], sizeof(DONNEE));
         pthread_create(&ThreadSecondaire[i], NULL, (void *(*)(void *))fonctionThread, &Param);
+    
     }
 
-    
     for (int i = 0; i < 4; i++)
     {
-        pthread_join(ThreadSecondaire[i], NULL);
-        printf("(Thread Principal %u) Thread esclave %d terminé.\n", pthread_self(), i+1);
+        pthread_join(ThreadSecondaire[i],NULL);
+        printf("(Thread Principal %u) Thread  %d terminé.\n", pthread_self(), i+1);
+
     }
 
     return 0;
@@ -53,9 +63,14 @@ int main()
 
 void *fonctionThread(DONNEE *param)
 {
+    char Nom[20];
+    strcpy(Nom, param->nom);
+    int Seconde = param->nbSecondes;
+
     /*
       * On affiche la création du thread 
       * On affiche le PID, le TID, le Nom et le nombre de seconde a patiénte
+      * On unlock le mutex pour que dès qu'on a afficher le dis thread, pour que pendant l'attente bah dcp ca fonctionne 
       * On initialise une structure timespec pour le nanosleep,
       * On quitte la fonction en ne retournant rien
     */
@@ -70,9 +85,12 @@ void *fonctionThread(DONNEE *param)
     struct timespec duree;
     duree.tv_sec = param->nbSecondes; // Secondes
 
-    nanosleep(&duree,NULL);
 
+    pthread_mutex_unlock(&MutexParam);
+    nanosleep(&duree,NULL);
+    fprintf(stderr, "(ThreadSecondaire %d.%u) \t Le thread secondaire %s a bien fini d'attendre %d  \n",getpid(), pthread_self(), Nom,Seconde);
 
     pthread_exit(NULL);
+
 
 }
---
+++ Exercice 3/Etape4.c
@@ -0,0 +1,170 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <string.h>
+
+
+
+typedef struct                          //? création d'une structure donnée, qui aura un nom et un nombre de seconde
+{
+    char nom[20];
+    int nbSecondes;
+} DONNEE;
+
+
+DONNEE data[] = {"MATAGNE", 15,         //? création d'une structure donnée, qui aura un nom et un nombre de seconde
+                 "WILVERS", 10,
+                 "WAGNER", 17,
+                 "QUETTIER", 3,
+                 "", 0};
+
+DONNEE Param;                           //? Variable globale qui permettra d'envoyer les données dans la fonction/
+
+
+
+pthread_t ThreadSecondaire[4];          //? Handler Des thread Secondaire
+
+
+
+pthread_mutex_t MutexParam;             //? Initialisation du mutex pour proteger la variable globale Param
+pthread_mutex_t MutexCompteur;          //? Initialisation du mutex pour proteger la variable globale compteur lors des incrémentations
+
+pthread_cond_t condCompteur;            //? Variable qui permettra de reconnaitre une conditions
+
+pthread_key_t cle;
+
+
+int compteur;                           //? Variable globale qui permet de savoir combien de Thread secondaire on a 
+
+
+void *fonctionThread(DONNEE *param);    // ?Fonction qui va être éxécuté par les différents thread
+
+int main()
+{
+    /* 
+    ? Fonction Main  
+     * Initialisation des différents mutex, et variable de conditions
+     * Boucle de création
+        * Ouverture d'un mutex pour protégé la variable globale Param 
+        * On copie la structure de DONNEE[i] dans une variable nommer param,
+        * Création des 4 threads secondaire en envoyant cette structure param
+     
+     * On lock la variable Compteur
+     * Boucle vérification de si il reste des Thread vivant, si positive, alors on attend(quelqu'un est encore vivant) sinon tt le monde est mort
+        * On attendra que la conditions soit envoyé,    
+     * On unlock le thread 
+
+     * on quitte le programme avec return 0
+    */
+
+
+    pthread_key_create(&cle,NULL);
+
+
+    // Initialisation des différents mutex, et variable de conditions
+    pthread_mutex_init(&MutexParam,NULL);
+    pthread_mutex_init(&MutexCompteur,NULL);
+    pthread_cond_init(&condCompteur, NULL);
+
+
+    // Création des différents Thread avec une protection de param
+    fprintf(stderr,"Création des Threads secondaire \n\n");
+    for (int i = 0; i < 4; i++)
+    {
+        pthread_mutex_lock(&MutexParam);         
+        memcpy(&Param, &data[i], sizeof(DONNEE));
+        pthread_create(&ThreadSecondaire[i], NULL, (void *(*)(void *))fonctionThread, &Param);
+    }
+
+
+    // Notification de morts des différentes Thread, avec une protection du compteur
+    char NomTuer[20];
+    pthread_mutex_lock(&MutexCompteur);
+    while(compteur > 0)
+    {
+        pthread_cond_wait(&condCompteur,&MutexCompteur);
+        strcpy(NomTuer,((char*)pthread_getspecific(cle)));
+
+        fprintf(stderr,"(Thread principale %u) le thread %s est mort \n",pthread_self(),NomTuer);
+    }
+    pthread_mutex_unlock(&MutexCompteur);
+
+
+    //Affichage que tout le monde est mort 
+    fprintf(stderr,"Tous le monde est mort \n");
+
+    return 0;
+}
+
+
+
+    /* 
+    ? Fonction Thread
+      * On lock le compteur pour l'incrementer
+      * On affiche l'identité du thread,
+      * On sauvegarde le param pour l'afficher lors de la mort
+      * On initialise la structure duree pour le nanosleep
+      * On unlock le mutexParam
+      * On attend x seconde
+      * On affiche que le Thread a fini d'attendre
+      * On lock le compteur et on le décremente
+      * Si le compteur est positive
+        * On envoie un signale pour activé la conditions dans le main
+      * On quitte la fonction main
+    */
+
+void *fonctionThread(DONNEE *param)
+{   
+    // On lock le compteur pour l'incrementer
+    pthread_mutex_lock(&MutexCompteur);
+    compteur++;
+    pthread_mutex_unlock(&MutexCompteur);
+
+    // On affiche l'identité du thread,
+    fprintf(stderr, "---------------\n");
+    fprintf(stderr, "(ThreadSecondaire %d.%u) \t Le thread secondaire a bien été crée \n", getpid(), pthread_self());
+    fprintf(stderr, "  - TID :  %u \n  - PID :  %d \n  - NOM :  %s \n  - SEC :  %d seconde\n", pthread_self(), getpid(),param->nom, param->nbSecondes);
+    fprintf(stderr, "---------------\n\n");
+
+    //On sauvegarde le param pour l'afficher lors de la mort
+    char *Nom = (char *)malloc(strlen(param->nom) + 1);
+    if (Nom == NULL) {
+        fprintf(stderr, "Erreur d'allocation mémoire.\n");
+        pthread_exit(NULL);  // Si l'allocation échoue, quitter le thread
+    }
+
+    strcpy(Nom, param->nom); // Copier le nom dans l'espace alloué
+
+    if (pthread_setspecific(cle, (void *)Nom));
+
+    int Seconde = param->nbSecondes;
+
+
+    //On initialise la structure duree pour le nanosleep
+    struct timespec duree;
+    duree.tv_sec = param->nbSecondes; // Secondes
+
+
+    //On unlock le mutexParam
+    pthread_mutex_unlock(&MutexParam);
+
+
+
+    //On attend x seconde
+    nanosleep(&duree,NULL);
+
+    fprintf(stderr, "(ThreadSecondaire %d.%u) \t Le thread secondaire %s a bien fini d'attendre %d  \n",getpid(), pthread_self(), Nom,Seconde);
+
+
+    //On lock le compteur et on le décremente
+    pthread_mutex_lock(&MutexCompteur);
+    compteur--;
+    pthread_cond_signal(&condCompteur);  //On envoie un signale pour activé la conditions dans le main
+    pthread_mutex_unlock(&MutexCompteur);
+
+    // On quitte le thread
+    pthread_exit(NULL);
+
+}
